d6933f3e177f5016812b82997cc20c09b0fdc81a
NikolaiZS <goodnightbozos@gmail.com>
2025-12-26 14:01:02 +0300

    EF Tools -> Supabase sdk

--- MetaFlow/MetaFlow.Api/Program.cs
+++ MetaFlow/MetaFlow.Api/Program.cs
@@ -1,15 +1,23 @@
-using MetaFlow.Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
+using MetaFlow.Infrastructure.Services;
+using MetaFlow.Infrastructure.Services.SupabaseServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<MetaFlowDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Supabase")));
+builder.Services.AddSingleton<SupabaseService>(sp =>
+{
+    var config = sp.GetRequiredService<IConfiguration>();
+    var url = config["Supabase:Url"]!;
+    var key = config["Supabase:ServiceRoleKey"]!;
+    return new SupabaseService(url, key);
+});
 
 var app = builder.Build();
+
+var supabaseService = app.Services.GetRequiredService<SupabaseService>();
+await supabaseService.InitializeAsync();
 
 if (app.Environment.IsDevelopment())
 {
@@ -20,5 +28,32 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapGet("/", () => "MetaFlow API is running!");
+
+app.MapGet("/test-supabase", async (IConfiguration config) =>
+{
+    try
+    {
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("apikey", config["Supabase:ServiceRoleKey"]!);
+        httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {config["Supabase:ServiceRoleKey"]}");
+
+        var url = $"{config["Supabase:Url"]}/rest/v1/";
+        var response = await httpClient.GetAsync(url);
+
+        return Results.Ok(new
+        {
+            status = "connected",
+            clientInitialized = true,
+            apiAccessible = response.IsSuccessStatusCode,
+            statusCode = (int)response.StatusCode
+        });
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
+
 
 app.Run();
---
+++ MetaFlow/MetaFlow.Infrastructure/Services/SupabaseServices/SupabaseService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supabase;
+
+namespace MetaFlow.Infrastructure.Services.SupabaseServices
+{
+    public class SupabaseService
+    {
+        public Supabase.Client Client { get; }
+
+        public SupabaseService(string url, string serviceRoleKey)
+        {
+            var options = new SupabaseOptions
+            {
+                AutoRefreshToken = true,
+                AutoConnectRealtime = true,
+            };
+
+            Client = new Supabase.Client(url, serviceRoleKey, options);
+        }
+
+        public async Task InitializeAsync()
+        {
+            await Client.InitializeAsync();
+        }
+    }
+
+
+}
